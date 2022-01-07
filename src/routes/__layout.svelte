3a6b22156fa7535ce76806a241716dd605bcb5ca
Dimitri Pietersz <pietersz@gmail.com>
2022-01-07 21:24:50 +0100

    Initial init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script lang="js">
+	import '../app.css';
+	import '../tailwind.css';
+</script>
+
+<div class="container mx-auto my-6 max-w-lg"><slot /></div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/**/*.{html,css,svelte}'],
+  content: ['./src/**/*.{html,css,svelte'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
